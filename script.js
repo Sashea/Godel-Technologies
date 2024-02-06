00578621649155e0ca0c9984e940ba5688f3ac14
Sashea <ghosteye519@gmail.com>
2024-02-06 11:50:18 +0100

    final version

---
+++ script.js
@@ -0,0 +1,34 @@
+function init() {
+    const audioElement = document.getElementById('musicPlayer');
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const source = audioContext.createMediaElementSource(audioElement);
+    const analyser = audioContext.createAnalyser();
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
+
+    const gridContainer = document.getElementById('grid');
+    const cells = gridContainer.querySelectorAll('#cell');
+    const numCells = cells.length;
+
+    function updateColors() {
+        const dataArray = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(dataArray);
+        cells.forEach((cell, index) => {
+            const frequencyIndex = Math.floor(index / numCells * dataArray.length);
+            const intensity = dataArray[frequencyIndex] / 255;
+            cell.style.backgroundColor = `rgba(141, 242, 114, ${intensity})`;
+        });
+        requestAnimationFrame(updateColors);
+    }
+    updateColors();
+    function playMusic() {
+        const fileInput = document.getElementById('musicFile');
+        const file = fileInput.files[0];
+        const objectURL = URL.createObjectURL(file);
+        audioElement.src = objectURL;
+        audioElement.play();
+    }
+    const fileInput = document.getElementById('musicFile');
+    fileInput.onchange = playMusic;
+}
+window.onload = init;
